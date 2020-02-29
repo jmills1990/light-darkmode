8307814aa77afbc09f385bb9d0731d989b2efcd4
jmills1990 <47228413+jmills1990@users.noreply.github.com>
2020-02-28 19:32:13 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ let slider = document.getElementById('myRange');
 slider.oninput = () => {
     document.body.style.background = `linear-gradient(90deg, #2b2e43 ${slider.value}%, #2b2e43 ${slider.value}%, #ffffff ${slider.value}.1%, #ffffff 100%)`;
 }
-
+done
