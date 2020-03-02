3c3ec40f4444492affe6a9ae0414b8e0da2f12ee
jmills1990 <47228413+jmills1990@users.noreply.github.com>
2020-03-01 19:07:27 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ slider.oninput = () => {
     document.body.style.background = `linear-gradient(90deg, #2b2e43 ${slider.value}%, #2b2e43 ${slider.value}%, #ffffff ${slider.value}.1%, #ffffff 100%)`;
 }
 
-let test = document.getElementById('cool');
 
-time to get some changes
+function clickCounter() {
+    if (typeof (Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount) + 1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } else {
+        document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+}
